93a7a11a533caf274140cac522c2d4f4d7f8075c
Louis Bourque <louis@louisbourque.ca>
2023-04-05 23:22:38 -0600

    Use Hash history mode for static gh pages compatibility

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import BoardView from '../views/BoardView.vue'
 import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
